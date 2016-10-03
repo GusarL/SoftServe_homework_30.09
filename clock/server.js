5114f00ccf26ce0f4a0a9533b4238f9e500cfc78
GusarL <lyudmila.gusar@mail.ru>
2016-10-03 21:42:34 +0300

    New server in clock (server gets index.html). Add ajax.js in Student&Palitra

--- clock/server.js
+++ clock/server.js
@@ -1,19 +1,37 @@
 'use strict';
 var http = require("http"),
     url = require("url"),
-    querystring = require('querystring');
+    querystring = require('querystring'),
+    fs = require('fs'),
+    baseRoot = './';
 
 http.createServer(function(request, response) {
     var pathname,
         minute,
+        path,
         date,
         hour;
     
     date = new Date();
     hour = date.getHours();
     minute = date.getMinutes();
+    path = baseRoot + request.url;
+
+    fs.exists(path, function(exists) {
+        if (!exists) {
+            response.writeHead(404);
+            response.write('Bad request');
+            response.end();
+        } else {
+            var file = fs.createReadStream(path);
+
+            file.on('open', function (){
+                file.pipe(response);
+            });
+        }
+    });
     
-    if (request.url === '/getTime') {
+    if (url.parse(request.url).pathname === '/getTime') {
         response.writeHead(200, {"Content-Type": "text/plain"});
 		response.end(add0(hour) + ":" + add0(minute));
     }
